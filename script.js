a02bc81115210a5ddff6f459680625032ef6f925
Rolex Oduor <roduor@gmail.com>
2025-08-03 01:15:42 +0300

    update script.js

--- script.js
+++ script.js
@@ -5,3 +5,5 @@
 ["addTask", "taskInput.value.trim()", "taskText", "alert"]
 
 ["addTask", "taskInput.value.trim()", "taskText", "alert"]
+
+["document.createElement", "button", "Remove", "remove-btn", "classList.add", "appendChild"]
